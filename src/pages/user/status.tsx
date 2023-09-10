419c8365f42f13ef5b6d0208bdb926b8dcb71062
vzsky <my999n@gmail.com>
2023-09-11 02:18:25 +0900

    status landing

--- src/pages/user/status.tsx
+++ src/pages/user/status.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from "next";
 import Layout from "../../components/layout";
+import { Heading, Text } from "@chakra-ui/react";
 
 const MyTeam: NextPage = () => {
   return (
     <Layout>
-      Welcome to the status page
+      <Heading> Status </Heading>
+      <Text> Status page is under construction. Come back later to check the registration progress. </Text>
     </Layout>
   )
 }
